47c7983dbfc1155c1170ae3c07018b25410326c9
ZhongXiangWong <e0725929@u.nus.edu>
2023-03-07 22:30:42 +0800

    Add help feature

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,41 @@
 package seedu.duke;
 
+import seedu.duke.command.Command;
+import seedu.duke.exception.CommandNotRecognisedException;
+
 import java.util.Scanner;
 
 public class Duke {
+
+    private final Ui ui;
+    private final Parser parser;
+
+    public Duke() {
+        ui = new Ui();
+        parser = new Parser();
+    }
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        new Duke().run();
     }
+
+    public void run() {
+        ui.printStartMessage();
+
+        String userInput;
+        do {
+            userInput = ui.getUserInput();
+            try {
+                parser.processCommand(userInput);
+            } catch (CommandNotRecognisedException e) {
+                ui.printCommandNotRecognised();
+                ui.printDivider();
+            }
+        } while (!userInput.equals(Command.COMMAND_BYE));
+
+    }
+
 }

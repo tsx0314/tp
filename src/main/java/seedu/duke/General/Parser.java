3f6ae7739f574ae9b0998957ccdf84fe03856b67
wanjuin <wanjuin2310@gmail.com>
2023-03-10 00:08:29 +0800

    group into packages

--- src/main/java/seedu/duke/General/Parser.java
+++ src/main/java/seedu/duke/General/Parser.java
@@ -1,4 +1,8 @@
-package seedu.duke;
+package seedu.duke.General;
+
+import seedu.duke.Commands.Command;
+import seedu.duke.Commands.ExitCommand;
+import seedu.duke.Exceptions.DukeException;
 
 public class Parser {
     public static Command parse(String fullCommand) throws DukeException {

e6082e7b0abaacf91120c72a617e1716e491bf4e
wanjuin <wanjuin2310@gmail.com>
2023-03-10 11:07:15 +0800

    -

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,12 +1,17 @@
 package seedu.duke;
 
 import seedu.duke.commands.Command;
+import seedu.duke.commands.CommandResult;
 import seedu.duke.exceptions.DukeException;
 import seedu.duke.general.Parser;
 import seedu.duke.general.Ui;
 
 import java.util.Scanner;
 
+/**
+ * Entry point of the Food Supply Tracker application
+ * Initializes the application and starts the interaction with the user.
+ */
 public class Duke {
     private Ui ui;
 
@@ -18,17 +23,16 @@ public class Duke {
     //Code below adapted from https://nus-cs2113-ay2223s2.github.io/website/schedule/week7/project.html
     public void run() {
 
-        ui.printOpeningMessage();
+        ui.showWelcomeMessage();
         boolean isExit = false;
-
-        Scanner sc = new Scanner(System.in);
 
         while (!isExit) {
             try {
                 String fullCommand = ui.readCommand();
                 ui.showLine();
-                Command c = Parser.parse(fullCommand);
-                c.execute(ui);
+                Command c = Parser.parseCommand(fullCommand);
+                CommandResult result = c.execute();
+                ui.showResultToUser(result);
                 isExit = c.isExit();
             } catch (DukeException e) {
                 ui.showError(e.getMessage());

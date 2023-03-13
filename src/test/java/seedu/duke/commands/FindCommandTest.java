8e53efb4b0d058a8ed2eba74197f7ae1f8f6ec2e
david <davydviniarskyi.dev@gmail.com>
2023-03-13 22:00:06 +0800

    find command test

---
+++ src/test/java/seedu/duke/commands/FindCommandTest.java
@@ -0,0 +1,21 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.food.Food;
+import seedu.duke.food.FoodList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindCommandTest {
+
+    @Test
+    public void termSearch_commandString_NumberOfFoundFood() {
+        FoodList foodList = new FoodList();
+        foodList.addFood(new Food("Chicken egg", "25/04/2022"));
+        foodList.addFood(new Food("Ostrich Egg  ", "26/04/2022"));
+        foodList.addFood(new Food("Cow's Milk", "26/04/2022"));
+        int expected = 2;
+        assertEquals(expected, foodList.findFood("EGG ").getNumberOfFood());
+    }
+
+}

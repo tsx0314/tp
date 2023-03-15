7abb7d3d7938fc4752fd01243ec9e0ef38a6a120
Sebastian Soewanto <sebastiansoewanto@outlook.com>
2023-03-15 11:10:40 +0800

    Add J unit test for remove command

---
+++ src/test/java/seedu/duke/commands/RemoveCommandTest.java
@@ -0,0 +1,22 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.DukeException;
+import seedu.duke.food.Food;
+import seedu.duke.food.FoodList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveCommandTest {
+
+    @Test
+    public void removeIndex_commandString_numberOfFoodLeft() {
+        FoodList foodList = new FoodList();
+        foodList.addFood(new Food("Milk", "25/03/2023"));
+        foodList.addFood(new Food("Bread", "18/03/2023"));
+        foodList.addFood(new Food("Cheese", "26/06/2023"));
+        foodList.removeFood(2);
+        int expectedOutput = 2;
+        assertEquals(expectedOutput, foodList.getNumberOfFood());
+    }
+}

ab497d9f9f4f8f1a271722ca992565ac448c2abc
ZhongXiangWong <e0725929@u.nus.edu>
2023-03-28 19:30:03 +0800

    Made category compulsory during adding

--- src/main/java/seedu/duke/commands/HelpCommand.java
+++ src/main/java/seedu/duke/commands/HelpCommand.java
@@ -16,7 +16,7 @@ public class HelpCommand extends Command {
     // + "\nAppend the filter '--fresh' for listing unexpired food products and "
     // + "'--expired' for listing expired food products.";
     private static final String HELP_ADD = "Command 'add': This command adds a food product to the food supply tracker."
-            + "\nFormat: add -n PRODUCT_NAME -e EXPIRY_DATE {-c category} {-q QUANTITY -u QUANTITY_UNIT}"
+            + "\nFormat: add -n PRODUCT_NAME -e EXPIRY_DATE -c category {-q QUANTITY -u QUANTITY_UNIT}"
             + "\nFormat for EXPIRY_DATE: DD/MM/YYYY"
             + "\nIdentifier within brackets are optional"
             + "\nList of valid categories as follows: fruit, meat, vegetable, dairy, grain, seafood, beverage, others"
